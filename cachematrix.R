a54d14d5432fb1ce6ee14a4c8231534c93efad63
MisterTom <tom.bratcher@btinternet.com>
2014-10-26 12:05:35 +0000

    Update cachematrix.R
    My functions!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Two functions below makeCachematrix and cacheSolve allow calculation of a matrix inverse
+# And recall of said inverse if and only if it's already been calculated.
 
-## Write a short comment describing this function
+# 1) makeCacheMatrix
+# makeCacheMatrix takes a matrix 'x' ,creates four simple subfunctions with
+# associated memory caches # in the global environment, and returns them as a list
+# which is associated with the current value of x.
+# The purpose of these subfunctions are commented within CacheMatrix
+# CacheMatrix doesn't, for example, calculate the inverse of x, but it
+# creates an instruction to do so if necessary 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+mm <- NULL			# so if we're running this for the very first time we 
+				# set mm to null - this is needed in CacheSolve as 
+				# this looks for mm and calls a calculation if mm is null
+        setm <- function(y) {
+                x <<- y
+                mm <<- NULL
+        }		# this subfunction records the current value of x and records the
+			# fact that its index isn't set
+        getm <- function() x	#this simply returns the matrix we started with 
+        setinv <- function(solve) mm <<- solve  # this function provides 
+								# a space in the global environment
+								# for a calculated inverse to be stored
+        getinv <- function() mm			# if we have an inverse, go and get it.
+						
+        list(setm = setm, getm = getm,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# 2) cacheSolve
+# cacheSolve acts on the function lists created by makeCacheMatrix 
+# This causes R to look for an existing inverse relating to the matrix in question
+# and either return it or calculate and store it.
+# NB makeCachematrix (x) must be defined as a variable in the global environment
+# and cacheSolve executed as a function of said variable
+# otherwise R thinks it's calculating a fresh inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+		
+	  mm <- x$getinv()
+
+	# Note that if we wanted error debugging we could add the original matrix 
+	# as an additional function argument (...,matrix,...) and 
+	# put the error checking in here
+	# eg "if(det(matrix)==0) {stop ("error:  x is not an invertible matrix")}"
+
+
+        if(!is.null(mm)) {
+                message("getting cached data")
+                return(mm)
+        }
+               data <- x$getm()
+
+         mm <- solve(data, ...)
+
+	  x$setinv(mm)
+        mm
 }
